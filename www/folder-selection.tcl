b3c33e6bc9db3d3d287d08a03bf2f9ab7c8a2832
Kenneth Knutsson <kenknut@algonet.se>
2001-08-15 05:40:35 +0000

    Syncing with server after move; adding fixes to CONNECT BY replacements. Presently, some are broken once multiple users are added to site

--- www/folder-selection.tcl
+++ www/folder-selection.tcl
@@ -8,5 +8,5 @@ set package_id [ad_conn package_id]
 set root_folder_id [bm_get_root_folder_id $package_id $viewed_user_id]
 
 # Get the folder list
-bm_folder_selection $viewed_user_id $bookmark_id $folder_p
+bm_folder_selection $viewed_user_id $root_folder_id $folder_p
 
